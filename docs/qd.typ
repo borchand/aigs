75ec0392c274a7d94182ad38131c426af911ec8c
Noah Syrkis <noah@syrkis.com>
2025-09-16 13:26:46 +0200

    qd

--- docs/qd.typ
+++ docs/qd.typ
@@ -9,21 +9,77 @@
 #align(center, text(1.5em, tracking: 0.1em, upper[Quality Diversity]))
 #v(2em)
 
-Quality diversity algorithms, rather than finding a specific good solution to a
-problem, construct archive of different high performing solutions. "Different"
-can be defined in various ways, an obvious example being "solutions that look
-different to a vision model" @kumar2024. A popular#footnote[At least in this
-  lab] quality diversity algorithm is the pedagogically named Multi-dimensional
-Archive of Phenotypic Elites (MAP-Elites) @mouret2015. Think of it as having a
-fitness dimension, on which we maximize, and (a) behavior dimension(s) which we
-want to populate.
+Quality diversity algorithms, rather than finding a _single_ good solution to a
+problem, constructs an archive of _different_ high performing solutions.
+"Different" can be defined in various ways, an obvious—and only recently
+possible—example being solutions that _look_ different to a vision model
+@kumar2024. A popular#footnote[At least amongst the researches teaching you]
+quality diversity algorithm is the pedagogically named "Multi-dimensional
+Archive of Phenotypic Elites" (MAP-Elites) @mouret2015. Think of it as having a
+fitness dimension, on which we maximize, and a set of behavioral dimensions we
+want to cover/explore.
+
+= Evolutionary algorithms
+
+Today (September 16th, 2025) we will be playing with evolutionary algorithms, as
+a way to get ready for quality diversity algorithms. Evolutionary algorithms are
+at their core, extremely intuitive: randomly mutate, see what works, and then
+further mutate on that. Inspired by the frequent bifurcation of species into
+sexes, we can further mix parts of one good solution with another, combining
+them to get a new (perhaps even better solution). You will now:
+1. Select a test function for optimization #footnote[#link(
+    "https://en.wikipedia.org/wiki/Test_functions_for_optimization",
+  )]
+2. Implement it in python (and visualize it)
+3. Find its optimal solution using an evolutionary algorithm from the lecture
+4. Make some cool plots of the results (get creative)
+
+= PCGYM
+
+Recall that Gym @towers2024 is a framework for reinforcement learning
+environments, consisting of an `init` and a `step` method (the same as those in
+our `aigs/games.py` file). Recall further that procedural content generation
+(PCG) focuses on the _procedure_ that generates a given piece of content, rather
+than the content itself. To that effect we have made `pcgym` #box[(itself
+  derived from `pcgrl` @khalifa2020)] that enables quick ideation of levels,
+supporting the kind of methods this lab is meant to have you play with. You will
+now:
+1. Explore `pcgym` #footnote[My fork of `gym-pcgrl` modified to work with our
+    course. It is located at #link(
+      "https://github.com/syrkis/pcgym",
+    ) but is already included in our environment] and have a random agent play a
+  level of any game.
+2. Replace the random agent with a _randomly initialized_ agent, that maps game
+  states to action.
+3. Improve the randomly initialized agent using the basic evolutionary algorithm
+  used in the previous task.
+4. Bonus: think about whether the cross-over operator makes sense for your
+  agent, and why / why not this is the case.
+
+= A\*
+
+A\* (A-star) is a pathfinding algorithm combining cost-so-far and estimated
+cost-to-goal, balancing shortest-path (like Dijkstra) with goal-directed search
+(like greedy best-first). In games like Mario, A\* is used to guide agents
+efficiently through levels by evaluating possible moves via a cost function
+(e.g., distance, obstacles) and a heuristic (e.g., estimated steps to the flag),
+generating optimal or near-optimal action sequences toward the goal. You will
+now:
+1. Think a bit about A\* (what it is and how you would implement it)
+2. Implement A\* or find an implementation online and have it play a level from
+  `pcgym`
+
 
 = Content generation
 
-We can optimize levels, just like we can optimize players. You must now:
-- Define a fitness function for a level
-- Define a behavior space for a level
-
+We can optimize levels, just like we can optimize players. Thinking about what
+it means for a level to be "fit", you must now:
+1. Define a fitness function for a level in `pcgym` (A\* can play a role in
+  evaluating a level)
+2. Define two _behavioral_ dimensions, meaning ways in which levels can be
+  different, that are _not_ fitness related (e.g., number of jumps).
+3. Generate an archive of good levels that are different from one another with
+  (Timothée's beloved) MAP-Elite algorithm.
 
 
 #[
--- labs/qd.py
+++ labs/qd.py
@@ -4,52 +4,101 @@
 
 # Imports
 import numpy as np
-from pcgym.envs import PcgrlEnv
-from typing import Tuple
-from pcgym.envs.helper import get_string_map
-
-# import qdax
-# from qdax.core.map_elites import MAPElites
-from PIL import Image
-# from qdax.core.emitters.mutation_operators import polynomial_mutation
+from functools import partial
+import matplotlib.pyplot as plt
 
 
-# %% Init population (maps)
-def init(cfg) -> Tuple[PcgrlEnv, np.ndarray]:
-    env = PcgrlEnv(prob=cfg.qd.game, rep=cfg.qd.rep, render_mode="rgb_array")
-    env.reset()
-    pop = np.random.randint(0, env.get_num_tiles(), (cfg.qd.n, *env._rep._map.shape))  # type: ignore
-    return env, pop
+# %% n-dimensional function with a strange topology
+@partial(np.vectorize, signature="(d)->()")
+def griewank_function(x):  # this is kind of our fitness function (except we a minimizing)
+    return 1 + np.sum(x**2) / 4000 - np.prod(np.cos(x / np.sqrt(np.arange(1, x.size + 1))))
 
 
-# %% using gym-pcg to evaluate map quality
-def eval(env, p) -> Tuple[int, np.ndarray]:
-    env._rep._map = p
-    string_map = get_string_map(env._rep._map, env._prob.get_tile_types())
-    stats = env._prob.get_stats(string_map)
-    behavior = np.array([stats["disjoint-tubes"], stats["empty"]])
-    return env._prob.get_reward(stats, {k: 0 for k in stats.keys()}), behavior
+@partial(np.vectorize, signature="(d)->(d)", excluded=[0])
+def mutate(sigma, x):  # What are we doing here?
+    return x + np.random.normal(0, sigma, x.shape)
 
 
-def mutate(cfg, env, p) -> np.ndarray:
-    mask = np.random.random(p.shape) < cfg.qd.p
-    p[mask] = np.random.randint(0, env.get_num_tiles(), p[mask].shape)
-    return p
+@partial(np.vectorize, signature="(d),(d)->(d)")
+def crossover(x1, x2):  # TODO: think about what we are doing here. Is it smart?
+    return x1 * np.random.rand() + x2 * (1 - np.random.rand())
 
 
-def archive(state, p):
-    pass
+def step(x, cfg):
+    fitness = griewank_function(x)
+    idxs = np.argsort(fitness)[: int(cfg.population * cfg.proportion)]  # select best
+    seed = np.tile(x[idxs], (int(cfg.population * cfg.proportion), 1))  # cross over
+    x = crossover(seed, seed[np.random.permutation(seed.shape[0])])  # mutate
+    return mutate(cfg.sigma, x), fitness  # return new generation and fitness
 
 
 # %% Setup
-def main(ctx):
-    print("9")
-    exit()
-    env, pop = init(ctx.config)
-    env._rep._mep = pop[0]
-    Image.fromarray(env.render()).save("map.png")
-    fitness, behavior = map(np.array, zip(*list(map(lambda p: eval(env, p), pop))))
-    print(behavior.shape, fitness.shape)
-    # print(fitness)
-    # print(behavior)
-    # print(list(map(lambda p: mutate(ctx.config, env, p), pop)))
+def main(cfg):
+    x = np.random.rand(cfg.population, cfg.dimensions)
+    fitnesses = []
+    for gen in range(cfg.generation):
+        x, fitness = step(x, cfg)
+        fitnesses.append(fitness.min())
+        print(f"Generation {gen}: Best fitness = {fitness.min()}")
+
+    plt.plot(fitnesses)
+    plt.yscale("log")
+    plt.xlabel("Generation")
+    plt.ylabel("Best Fitness")
+    plt.show()
+
+
+# %% Plotting function that I think we should put in utils.py
+def plot(fn):
+    x1 = np.linspace(-10, 10, 100)
+    x2 = np.linspace(-10, 10, 100)
+    xs = np.stack(np.meshgrid(x1, x2), axis=-1)
+    ys = fn(xs)
+    plt.imshow(ys, cmap="viridis")
+    plt.colorbar()
+    plt.show()
+
+
+# env, pop = init(ctx.config)
+# from pcgym.envs import PcgrlEnv
+# from typing import Tuple
+# from pcgym.envs.helper import get_string_map
+
+# import qdax
+# from qdax.core.map_elites import MAPElites
+# from PIL import Image
+# from qdax.core.emitters.mutation_operators import polynomial_mutation
+# env._rep._mep = pop[0]
+# Image.fromarray(env.render()).save("map.png")
+# fitness, behavior = map(np.array, zip(*list(map(lambda p: eval(env, p), pop))))
+# print(behavior.shape, fitness.shape)
+# # print(fitness)
+# # print(behavior)
+# # print(list(map(lambda p: mutate(ctx.config, env, p), pop)))
+
+
+# # %% Init population (maps)
+# def init(cfg) -> Tuple[PcgrlEnv, np.ndarray]:
+#     env = PcgrlEnv(prob=cfg.qd.game, rep=cfg.qd.rep, render_mode="rgb_array")
+#     env.reset()
+#     pop = np.random.randint(0, env.get_num_tiles(), (cfg.qd.n, *env._rep._map.shape))  # type: ignore
+#     return env, pop
+
+
+# # %% using gym-pcg to evaluate map quality
+# def eval(env, p) -> Tuple[int, np.ndarray]:
+#     env._rep._map = p
+#     string_map = get_string_map(env._rep._map, env._prob.get_tile_types())
+#     stats = env._prob.get_stats(string_map)
+#     behavior = np.array([stats["disjoint-tubes"], stats["empty"]])
+#     return env._prob.get_reward(stats, {k: 0 for k in stats.keys()}), behavior
+
+
+# def mutate(cfg, env, p) -> np.ndarray:
+#     mask = np.random.random(p.shape) < cfg.qd.p
+#     p[mask] = np.random.randint(0, env.get_num_tiles(), p[mask].shape)
+#     return p
+
+
+# def archive(state, p):
+#     raise NotImplementedError
